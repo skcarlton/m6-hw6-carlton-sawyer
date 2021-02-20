c8055c4f6a72e0f156790ead2daf93062af56172
Sawyer Carlton <skcarlton@gmail.com>
2021-02-19 19:38:48 -0500

    Add appearing messages

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 var boxes = document.getElementsByClassName('boxes');
 var box = document.getElementsByClassName('box');
-var popUp = document.getElementsByClassName('messages');
+var popUpOne = document.getElementsByClassName('one');
+var popUpTwo = document.getElementsByClassName('two');
+var popUpThree = document.getElementsByClassName('three');
+var answers = document.getElementsByClassName('messages');
 
 function highlight () {
     for(i = 0; i < box.length; i++) {
@@ -12,3 +15,24 @@ for(i = 0; i < box.length; i++) {
     box[i].addEventListener('mouseenter', highlight);
     box[i].addEventListener('mouseleave', highlight);
 }
+
+//Answer #1
+function answer1 () {
+    answers[0].classList.remove('hide');
+}
+
+popUpOne[0].addEventListener("click", answer1);
+
+//Answer #2
+function answer2 () {
+    answers[1].classList.remove('hide');
+}
+
+popUpTwo[0].addEventListener("click", answer2);
+
+//Answer #3
+function answer3 () {
+    answers[2].classList.remove('hide');
+}
+
+popUpThree[0].addEventListener("click", answer3);
